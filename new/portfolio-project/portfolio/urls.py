a8bb98bed70103600a1a3586b5af49631e60c092
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-02 21:17:54 -0500

    portfolio/urls.py - add homepage

--- new/portfolio-project/portfolio/urls.py
+++ new/portfolio-project/portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from models import jobs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.homepage, name='home'),
 ]
