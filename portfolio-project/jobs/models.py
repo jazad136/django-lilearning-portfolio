9cf12cc063934d31d688590d751f2966f90b4c42
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-09 09:55:08 -0500

    Move portfolio-project to top level

---
+++ portfolio-project/jobs/models.py
@@ -0,0 +1,12 @@
+   # Copyright 2022 Jonathan A. Saddler
+   #
+   # Licensed under the Apache License, Version 2.0 (the "License");
+   # you may not use this file except in compliance with the License.
+   # You may obtain a copy of the License at
+   # http://www.apache.org/licenses/LICENSE-2.0 
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
