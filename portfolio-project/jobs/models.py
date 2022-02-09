d332fb8ed45e5e0c3d496e660eb913e7bba1c569
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-09 10:32:54 -0500

    Pull summary and display (notes below)
    File pickers do not work in some browsers.
    Be sure to launch edits to jobs using Firefox and not chrome.

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -3,7 +3,7 @@
    # Licensed under the Apache License, Version 2.0 (the "License");
    # you may not use this file except in compliance with the License.
    # You may obtain a copy of the License at
-   # http://www.apache.org/licenses/LICENSE-2.0 
+   # http://www.apache.org/licenses/LICENSE-2.0
 from django.db import models
 
 # Create your models here.
