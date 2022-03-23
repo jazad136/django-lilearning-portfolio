acf65fb9fb98256c258e277d7fe54a897e464729
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-23 18:39:12 -0400

    [MODEL] (#2) Update Detail Page with Model View

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -4,7 +4,7 @@
    # you may not use this file except in compliance with the License.
    # You may obtain a copy of the License at
    # http://www.apache.org/licenses/LICENSE-2.0
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 # Create your views here.
 def homepage(request):
@@ -12,5 +12,6 @@ def homepage(request):
     return render(request, 'jobs/home.html', {'jobs': jobs})
 
 def detail(request, job_id):
-   print(job_id)
-   return render(request, 'jobs/home.html')
+   # print(job_id)
+   job_detail = get_object_or_404(Job, pk=job_id)
+   return render(request, 'jobs/detail.html', {'job': job_detail})
