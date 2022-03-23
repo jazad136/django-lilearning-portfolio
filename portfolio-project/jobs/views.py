056fa788b5b1426bdf249968fd140dc0a4bcaed1
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-23 18:55:24 -0400

    Home: Add Links to JobDetails album cards

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -7,6 +7,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Job
 # Create your views here.
+
 def homepage(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs': jobs})
